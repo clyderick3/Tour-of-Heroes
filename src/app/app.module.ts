31c74a7e7d119bf65e06d3e65d662f1d2276d8f5
Clyde Broderick <clyderick3@gmail.com>
2022-04-23 21:21:55 -0400

    Added httpclientmodule to module of app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
